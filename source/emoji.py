0237333d0c9d1eefa4158316554ee57485e4f422
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2017-06-24 18:57:02 +0300

    initial

---
+++ source/emoji.py
@@ -0,0 +1,20 @@
+# https://github.com/carpedm20/emoji
+# http://www.unicode.org/emoji/charts/full-emoji-list.html
+
+
+class Button:
+    Camera = u'\U0001F4F7'
+    FileFolder = u'\U0001F4C1'
+    FileFolderOpen = u'\U0001F4C2'
+    RightArrowUp = u'\U00002934'
+    RightArrowDown = u'\U00002935'
+    CrossMarkRed = u'\U0000274C'
+    CheckMark = u'\U00002714'
+    QuestionMark = u'\U00002754'
+    MarkWarning = u'\U000026A0'
+    NoEntry = u'\U000026D4'
+
+
+class Country:
+    UnitedKingdom = u'\U0001F1EC\U0001F1E7'
+    Russia = u'\U0001F1F7\U0001F1FA'
---
+++ source/i18n.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+from django.utils import translation
+
+COOKIE_NAME = 'lang'
+DEFAULT_CODE = 'en'
+LANGUAGES = {
+    'en': 'English',
+    'ru': 'Русский',
+}
+
+languages = [x for x in LANGUAGES.keys() if x != 'en']
+
+
+def get_lang(code):
+    if not code:
+        return ''
+    for l in languages:
+        if l in code:
+            return l
+    return ''
+
+
+def lang_for_user(from_user):
+    lang = get_lang(from_user.language_code)
+    if lang:
+        activate(lang)
+
+
+def activate(lang):
+    old = translation.get_language()
+    translation.activate(lang)
+    return old
+
+
+def ua_languages(http_accept_language, lang_list, lang_dflt):
+    lst = []
+    for itm in http_accept_language.split(','):
+        if ';' in itm:
+            itm = itm.split(';')[0]
+        if '-' in itm:
+            itm = itm.split('-')[0]
+        if itm not in lst:
+            lst.append(itm)
+
+    for lng in lst:
+        if lng in lang_list:
+            return lng
+
+    return lang_dflt
+
+
+class Context():
+    code = DEFAULT_CODE
+
+    def process_request(self, request):
+
+        if COOKIE_NAME in request.COOKIES.keys():
+            new_code = request.COOKIES[COOKIE_NAME]
+            if new_code in LANGUAGES.keys():
+                self.code = new_code
+        else:
+            self.code = ua_languages(
+              request.META.get('HTTP_ACCEPT_LANGUAGE', ''),
+              LANGUAGES.keys(),
+              DEFAULT_CODE
+            )
+
+        activate(self.code)
+        return None
+
+
+def template_context(request):
+    return {
+      'lang': translation.get_language(),
+      'lang_list': LANGUAGES,
+    }
