bfbcabbd282fe9a6e13a737ec2707f1266042688
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2017-07-05 15:11:44 +0400

    flake8

--- source/html2text.py
+++ source/html2text.py
@@ -1,36 +1,40 @@
-#http://softwaremaniacs.org/forum/django/14909/
-import HTMLParser
+# http://softwaremaniacs.org/forum/django/14909/
+from HTMLParser import HTMLParser
+from re import sub
 
-class _DeHTMLParser(HTMLParser.HTMLParser):
-  def __init__(self):
-    HTMLParser.__init__(self)
-    self.__text = []
 
-  def handle_data(self, data):
-    text = data.strip()
-    if len(text) > 0:
-      text = sub('[ \t\r\n]+', ' ', text)
-      self.__text.append(text + ' ')
+class _DeHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.__text = []
 
-  def handle_starttag(self, tag, attrs):
-    if tag == 'p':
-      self.__text.append('\n\n')
-    elif tag == 'br':
-      self.__text.append('\n')
+    def handle_data(self, data):
+        text = data.strip()
+        if len(text) > 0:
+            text = sub('[ \t\r\n]+', ' ', text)
+            self.__text.append(text + ' ')
 
-  def handle_startendtag(self, tag, attrs):
-    if tag == 'br':
-      self.__text.append('\n\n')
+    def handle_starttag(self, tag, attrs):
+        if tag == 'p':
+            self.__text.append('\n\n')
+        elif tag == 'br':
+            self.__text.append('\n')
 
-  def text(self):
-    return ''.join(self.__text).strip()
+    def handle_startendtag(self, tag, attrs):
+        if tag == 'br':
+            self.__text.append('\n\n')
+
+    def text(self):
+        return ''.join(self.__text).strip()
+
 
 def convert(text):
-  try:
-    parser = _DeHTMLParser()
-    parser.feed(text)
-    parser.close()
-    return parser.text()
-  except:
-    return text
+    try:
+        parser = _DeHTMLParser()
+        parser.feed(text)
+        parser.close()
 
+        return parser.text()
+
+    except Exception as e:
+        return str(e)
